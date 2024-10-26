1bc393635cc32a0d04c50f2c1b12c4e24c1a494f
Ignaz Kraft <ignaz.k@live.de>
2024-10-27 01:24:20 +0200

    first brain

---
+++ src/deciders/brain.py
@@ -0,0 +1,86 @@
+"""Brain decider."""
+import random
+import math
+
+from src import model
+
+
+def leaky_relu(x: float) -> float:
+    """LeakyReLU"""
+    if x < 0.0:
+        return x * 0.01
+    return x
+
+
+def softmax(x: list[float]) -> list[float]:
+    """SOFTMAX"""
+    tmp = [math.exp(v) for v in x]
+    total = sum(tmp)
+    return [t / total for t in tmp]
+
+
+def scale(x, i_min, i_max):
+    """Scale a number to between 0 and 1."""
+    return (x - i_min) / i_max
+
+
+def clamp(x: float):
+    """Clamp between -1 and 1"""
+    if x < -1.0:
+        return -1.0
+    if x > 1.0:
+        return 1.0
+    return x
+
+
+class BrainDecider(model.Decider):
+    """Use a NN to make the decision."""
+
+    def __init__(self, l1: int, l2: int):
+        self.l1w = l1
+        self.l2w = l2
+        self.w1 = [random.uniform(-1.0, 1.0) for _ in range(l1)]
+        self.b1 = [random.uniform(-1.0, 1.0) for _ in range(l1)]
+        self.w2 = [random.uniform(-1.0, 1.0) for _ in range(l2)]
+        self.b2 = [random.uniform(-1.0, 1.0) for _ in range(l2)]
+        self.w3 = [random.uniform(-1.0, 1.0) for _ in range(2)]
+        self.b3 = [random.uniform(-1.0, 1.0) for _ in range(2)]
+
+    def mutate(self, m: float):
+        """Some random mutation."""
+        other = BrainDecider(self.l1w, self.l2w)
+        other.w1 = [clamp(before + random.uniform(-m, m))
+                    for before in self.w1]
+        other.b1 = [clamp(before + random.uniform(-m, m))
+                    for before in self.b1]
+        other.w2 = [clamp(before + random.uniform(-m, m))
+                    for before in self.w2]
+        other.b2 = [clamp(before + random.uniform(-m, m))
+                    for before in self.b2]
+        other.w3 = [clamp(before + random.uniform(-m, m))
+                    for before in self.w3]
+        other.b3 = [clamp(before + random.uniform(-m, m))
+                    for before in self.b3]
+        return other
+
+    def decide(self, tank):
+        l0 = [
+            scale(tank.tank.l, 0, tank.tank_max.l),
+            tank.energy_price.get(tank.time)
+        ]
+        l1 = [
+            leaky_relu(sum(w0 * w1 for w0 in l0) + b1)
+            for w1, b1 in zip(self.w1, self.b1)
+        ]
+        l2 = [
+            leaky_relu(sum(w1 * w2 for w1 in l1) + b2)
+            for w2, b2 in zip(self.w2, self.b2)
+        ]
+        l3 = [
+            leaky_relu(sum(w2 * w3 for w2 in l2) + b3)
+            for w3, b3 in zip(self.w3, self.b3)
+        ]
+        l4 = softmax(l3)
+        if l4[0] > 0.5:
+            return 1.0
+        return 0.0
---
+++ src/evolution.py
@@ -0,0 +1,73 @@
+"""It's evolution baby!!!"""
+import datetime
+
+import pandas as pd
+from tqdm import tqdm
+
+from src import model
+from src.deciders.brain import BrainDecider
+from src.pumps.simple import SimplePump
+
+start_time = datetime.datetime.fromisoformat("2024-04-01T00:00Z")
+pump = SimplePump(model.KW(20), model.LiterPerSecond(
+    16.5), datetime.timedelta(minutes=15))
+drain = pd.read_csv("./data/drain.csv")
+
+prices = pd.read_csv("./data/price.csv")
+prices["datetime"] = prices["Tag"] + "T" + prices["Uhrzeit"]
+prices["datetime"] = pd.to_datetime(prices["datetime"].str.split(" ").str[0])
+price = model.DynamicPowerPrice(
+    prices["datetime"].to_list(), (prices["flexibel [ct/kWh]"] / 100).to_list())
+
+
+class Evolution:
+    """Use some genetic algorithm to find the best decider."""
+
+    def __init__(self, brains: list[BrainDecider] = None):
+        if not brains:
+            brains = [
+                BrainDecider(5, 5) for _ in range(10)
+            ]
+
+        self.tanks = [model.Tank(
+            start_time,
+            model.Liter(540_000), model.Liter(
+                540_000*0.1), model.Liter(540_000*0.5),
+            pump, price, decider
+        ) for decider in brains]
+
+    def run(self) -> list[BrainDecider]:
+        """Run one epoch."""
+        number = len(drain)
+        for index, volume in enumerate(tqdm(drain["Tank1"])):
+            for tank in self.tanks:
+                if tank.cost > 1_000:
+                    continue
+
+                tank.foreward(datetime.timedelta(minutes=15),
+                              model.LiterPerSecond(volume))
+                if (tank.tank < tank.tank_min or
+                        tank.tank > tank.tank_max):
+                    tank.cost += 1_001 * (number - index)
+
+        # for tank in self.tanks:
+        #     tank.cost += abs(tank.tank.l - (tank.tank_max.l / 2)) * 2
+
+        self.tanks.sort(key=lambda tank: tank.cost)
+        print(self.tanks[0].cost)
+        return [self.tanks[i].decider for i in range(3)]
+
+
+if __name__ == "__main__":
+    e = Evolution()
+    best = e.run()
+
+    for _ in range(10):
+        brains = []
+        brains.extend([x.mutate(0.0) for x in best])
+        brains.extend([x.mutate(0.25) for x in best])
+        brains.extend([x.mutate(0.5) for x in best])
+        brains.append(BrainDecider(5, 5))
+        assert len(brains) == 10
+        e = Evolution(brains)
+        best = e.run()
